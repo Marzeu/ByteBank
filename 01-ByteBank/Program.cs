a2caa764565a39a48cdd22ce6e750c32e27a17eb
Marzeu <marzahell@hotmail.com>
2021-03-17 16:55:44 -0300

    classe e objeto criado

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,9 +10,19 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            string titular = "Gabriela";
-            int numeroAgencia = 863;
-            int numero = 863146
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
         }
     }
 }

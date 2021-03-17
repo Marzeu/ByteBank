800e681ed7ce40e60315cc9562d592873894841b
Marzeu Selau Junior <mselauju@everis.com>
2021-03-17 15:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Gabriela";
+            int numeroAgencia = 863;
+            int numero = 863146
+        }
+    }
+}

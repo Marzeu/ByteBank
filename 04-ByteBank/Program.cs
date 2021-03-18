45fa3121f4527e2a8f0b37caebc8b642df6a7f3b
Marzeu <marzahell@hotmail.com>
2021-03-18 17:21:19 -0300

    classe contaCorrente criada

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(430);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaDoBruno.Depositar(430);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(100, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado da transferência: " + resultadoTransferencia);
+
+            contaDaGabriela.Transferir(200, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado da transferência: " + resultadoTransferencia);
+
+            Console.ReadLine();
+        }
+    }
+}
